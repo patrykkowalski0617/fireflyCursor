930f16046d1e117fa7571078df4a5036bd0ab17d
Patryk Kowalski <patrykkowalski0617@gmail.com>
2025-12-11 16:23:40 +0100

    second commit

--- content.js
+++ content.js
@@ -1,39 +1,56 @@
 (() => {
-  document.body.insertAdjacentHTML("afterbegin", `<div id="cursor"></div>`);
-  const cursor = document.querySelector("#cursor");
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `<div id="fireflyCursor"></div>`
+  );
+  const fireflyCursor = document.querySelector("#fireflyCursor");
 
   const positionElement = (e) => {
     const mouseY = e.clientY;
     const mouseX = e.clientX;
-    cursor.style.top = `${mouseY}px`;
-    cursor.style.left = `${mouseX}px`;
+    fireflyCursor.style.top = `${mouseY}px`;
+    fireflyCursor.style.left = `${mouseX}px`;
   };
 
-  const getVibrantColor = () => {
-    const brightLetters = "89ABCDEF";
+  const getCalmColor = () => {
+    const calmLetters = "9ABCDEF";
     let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += brightLetters[Math.floor(Math.random() * brightLetters.length)];
-    }
+    let component1 =
+      calmLetters[Math.floor(Math.random() * calmLetters.length)];
+    let component2 =
+      calmLetters[Math.floor(Math.random() * calmLetters.length)];
+
+    const lowLetters = "6789";
+    let lowComponent =
+      lowLetters[Math.floor(Math.random() * lowLetters.length)];
+
+    const parts = [
+      component1 + component1,
+      component2 + component2,
+      lowComponent + lowComponent,
+    ].sort(() => Math.random() - 0.5);
+
+    color += parts.join("");
     return color;
   };
+
   let timeoutId;
 
   const clickElement = () => {
-    const randomColor = getVibrantColor();
-    cursor.style.setProperty("--random-color", randomColor);
+    const randomColor = getCalmColor();
+    fireflyCursor.style.setProperty("--random-color", randomColor);
 
-    cursor.classList.remove("click");
+    fireflyCursor.classList.remove("click");
     clearTimeout(timeoutId);
 
-    void cursor.offsetWidth;
+    void fireflyCursor.offsetWidth;
 
-    cursor.classList.add("click");
+    fireflyCursor.classList.add("click");
+
     timeoutId = setTimeout(() => {
-      cursor.classList.remove("click");
-    }, 600);
+      fireflyCursor.classList.remove("click");
+    }, 850);
   };
-
   window.addEventListener("mousemove", positionElement);
   window.addEventListener("click", clickElement);
 })();
